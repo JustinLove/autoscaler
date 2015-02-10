5772be66e8d755f4178dcbfb084959b098c65ced
Chris Stringer <chris.stringer@g5searchmarketing.com>
2015-02-10 14:53:30 -0800

    Support for custom worker name

--- spec/autoscaler/heroku_scaler_spec.rb
+++ spec/autoscaler/heroku_scaler_spec.rb
@@ -2,48 +2,63 @@ require 'spec_helper'
 require 'autoscaler/heroku_scaler'
 require 'heroku/api/errors'
 
-describe Autoscaler::HerokuScaler, :online => true do
+describe Autoscaler::HerokuScaler do
   let(:cut) {Autoscaler::HerokuScaler}
   let(:client) {cut.new}
   subject {client}
 
-  its(:workers) {should == 0}
+  describe "initialization" do
+    let(:worker_name){ "not_worker" }
 
-  describe 'scaled' do
     around do |example|
-      client.workers = 1
+      ENV['SIDEKIQ_WORKER_NAME'] = worker_name
       example.yield
-      client.workers = 0
+      ENV['SIDEKIQ_WORKER_NAME'] = nil
     end
+    
+    its(:type) { should == worker_name }  
+  end   
 
-    its(:workers) {should == 1}
-  end
+  describe "online", :online => true do
+    its(:workers) {should == 0}
+    its(:type) { should == "worker" } 
 
-  shared_examples 'exception handler' do |exception_class|
-    before do
-      client.should_receive(:client){
-        raise exception_class.new(Exception.new('oops'))
-      }
-    end
-
-    describe "default handler" do
-      it {expect{client.workers}.to_not raise_error}
-      it {client.workers.should == 0}
-      it {expect{client.workers = 2}.to_not raise_error}
-    end
-
-    describe "custom handler" do
-      before do
-        @caught = false
-        client.exception_handler = lambda {|exception| @caught = true}
+    describe 'scaled' do
+      around do |example|
+        client.workers = 1
+        example.yield
+        client.workers = 0
       end
 
-      it {client.workers; @caught.should be_true}
+      its(:workers) {should == 1}
+    end
+
+    shared_examples 'exception handler' do |exception_class|
+      before do
+        client.should_receive(:client){
+          raise exception_class.new(Exception.new('oops'))
+        }
+      end
+
+      describe "default handler" do
+        it {expect{client.workers}.to_not raise_error}
+        it {client.workers.should == 0}
+        it {expect{client.workers = 2}.to_not raise_error}
+      end
+
+      describe "custom handler" do
+        before do
+          @caught = false
+          client.exception_handler = lambda {|exception| @caught = true}
+        end
+
+        it {client.workers; @caught.should be_true}
+      end
+    end
+
+    describe 'exception handling', :focus => true do
+      it_behaves_like 'exception handler', Excon::Errors::SocketError
+      it_behaves_like 'exception handler', Heroku::API::Errors::Error
     end
   end
-
-  describe 'exception handling', :focus => true do
-    it_behaves_like 'exception handler', Excon::Errors::SocketError
-    it_behaves_like 'exception handler', Heroku::API::Errors::Error
-  end
-end
+end  
