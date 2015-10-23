73b248abe94774f1dcd68cc5cd2e789abb1bc274
Justin Love <git@JustinLove.name>
2015-10-23 10:22:26 -0500

    clean up and fix specs that did not break earlier

--- spec/test_system.rb
+++ spec/test_system.rb
@@ -8,4 +8,10 @@ class TestSystem
   def queued; @pending; end
   def scheduled; 0; end
   def retrying; 0; end
+  def total_work
+    queued + scheduled + retrying + workers
+  end
+  def any_work?
+    queued > 0 || scheduled > 0 || retrying > 0 || workers > 0
+  end
 end
