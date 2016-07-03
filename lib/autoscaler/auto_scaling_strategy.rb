63ca02d8022c8311643cc0fa57c3d218906226b9
Pedro Bachiega <pedro@pedrobachiega.com>
2016-07-03 20:40:04 -0300

    new auto_scaling_client

--- lib/autoscaler/auto_scaling_strategy.rb
+++ lib/autoscaler/auto_scaling_strategy.rb
@@ -1,33 +1,22 @@
 module Autoscaler
   # Strategies determine the target number of workers
-  # The default strategy has a single worker when there is anything, or shuts it down.
+  # This strategy sets the number of workers to be proportional to the number of enqueued jobs.
   class AutoScalingStrategy
-    #@param [integer] active_workers number of workers when in the active state.
-    def initialize(jobs_limit = 100)
-      # p "@@@@@@@ Autoscaler::AutoScalingStrategy#new #{jobs_limit}"
-      @jobs_limit = jobs_limit
+    #@param [integer] max_workers     maximum number of workers to spin up.
+    #@param [integer] worker_capacity the amount of jobs one worker can handle
+    def initialize(max_workers = 1, worker_capacity = 50)
+      @max_workers = max_workers
+      @worker_capacity = worker_capacity
     end
 
     # @param [QueueSystem] system interface to the queuing system
     # @param [Numeric] event_idle_time number of seconds since a job related event
     # @return [Integer] target number of workers
     def call(system, event_idle_time)
-      # p "@@@@@@@ Autoscaler::AutoScalingStrategy#call #{jobs_count(system)}"
-      if active?(system)
-        ((jobs_count(system) - 1) / @jobs_limit) + 1
-      else
-        0
-      end
-    end
+      initial_workers = system.workers == 0 ? 1 : system.workers
+      workers_count = system.any_work? ? ((system.total_work - 1) / @worker_capacity) + 1 : initial_workers
 
-    private
-    def active?(system)
-      system.any_work?
-    end
-
-    def jobs_count(system)
-      system.total_work
+      return [workers_count, @max_workers].min
     end
   end
 end
-
--- lib/autoscaler/sidekiq.rb
+++ lib/autoscaler/sidekiq.rb
@@ -1,5 +1,7 @@
 require 'autoscaler/sidekiq/client'
 require 'autoscaler/sidekiq/thread_server'
+require 'autoscaler/sidekiq/autoscaler_client'
+require 'autoscaler/sidekiq/autoscaler_server'
 
 module Autoscaler
   # namespace module for Sidekiq middlewares
